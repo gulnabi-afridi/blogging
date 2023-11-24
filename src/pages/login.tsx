8e317c5412b28921cfdd6761a9c8aa2ae6832834
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-11-24 14:56:05 +0500

    add new component made

---
+++ src/pages/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Login from '@/components/login/Login';
+
+const login = () => {
+  return (
+    <div className='w-full h-full'>
+      <Login />
+    </div>
+  );
+};
+
+export default login;
