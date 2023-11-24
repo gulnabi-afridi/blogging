73d663124a72abdb3dca5eb6719244ad6e67c31b
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-11-24 14:59:11 +0500

    blog detail ui completed

---
+++ src/components/blogDetails/details/Details.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import BreadCrumb from './BreadCrumb';
+import Blog from './Blog';
+import Comments from '../comments/Comments';
+
+const Details = () => {
+  return (
+    <div className='w-full h-full flex flex-col items-center lg:items-start justify-start gap-10'>
+      <BreadCrumb />
+      <Blog />
+      <Comments />
+    </div>
+  );
+};
+
+export default Details;
