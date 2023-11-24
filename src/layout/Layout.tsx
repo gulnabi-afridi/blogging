4b80f4e53e8b1c18b46f741c3b7344c045ee0cde
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-11-24 14:32:33 +0500

    layout completed

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,7 +1,46 @@
+import Navabar from '@/components/shared/navigation/Navibar';
+import Footer from '@/components/shared/navigation/Footer';
 import React from 'react';
+import { useRouter } from 'next/router';
+import SideBar from '@/components/user/SideBar';
 
-const Layout = () => {
-  return <div>Layout</div>;
+interface Props {
+  children: React.ReactNode;
+}
+const Layout: React.FC<Props> = ({ children }: Props) => {
+  const router = useRouter();
+  const userLayout = router.pathname.includes('dashboard');
+  const renderNavbar = () => {
+    if (router.pathname.includes('dashboard')) {
+      return false;
+    } else return true;
+  };
+  const renderFooter = () => {
+    if (
+      router.pathname == '/login' ||
+      router.pathname == '/signup' ||
+      router.pathname.includes('/dashboard')
+    ) {
+      return false;
+    } else return true;
+  };
+  return (
+    <div className='w-full h-full'>
+      {renderNavbar() && <Navabar />}
+      {!userLayout && (
+        <main className='w-full h-full font-inter'>{children}</main>
+      )}
+      {userLayout && (
+        <div className='w-full h-full md:h-screen md:grid md:grid-cols-[320px,calc(100%-320px)] flex flex-col'>
+          <SideBar />
+          <main className='w-full h-full bg-[#F9F9F9] md:p-6 lg:p-10'>
+            {children}
+          </main>
+        </div>
+      )}
+      {renderFooter() && <Footer />}
+    </div>
+  );
 };
 
 export default Layout;
