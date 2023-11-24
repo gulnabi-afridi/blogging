5a284efa096aa7372412183e5844b5ba30087662
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-11-24 14:57:28 +0500

    comments Ui completed

---
+++ src/components/blogDetails/comments/Comments.tsx
@@ -0,0 +1,14 @@
+import React, { FormEvent } from 'react';
+import AddNewComment from './AddNewComment';
+import AllComments from './AllComments';
+
+const Comments = () => {
+  return (
+    <div className='w-full flex flex-col lg:items-start items-center justify-start gap-10'>
+      <AddNewComment />
+      <AllComments />
+    </div>
+  );
+};
+
+export default Comments;
