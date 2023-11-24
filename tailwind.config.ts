abcd7d5ee97ea9b33da71e794762fcc2bd29fb66
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-11-24 14:34:25 +0500

    tailwind config set

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,51 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './Layout/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'my-background-image': "url('/path/to/your/background-image.jpg')",
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      screens: {
+        xs: '325px',
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1280px',
+      },
+      colors: {
+        brand: {
+          main: '#1565D8',
+        },
+        white: {
+          main: '#FFFFFF',
+          off: '#F2F4F5',
+          cool: '#929299',
+        },
+        black: {
+          main: '#000000',
+          secondary: '#5A7184',
+          off: '#0D2436',
+        },
+        gray: {
+          main: '#EFEFEF',
+          cool: '#e4e6eb',
+          lightGray: '#f8f8f8',
+        },
+        red: {
+          main: '#FF0000',
+          secondary: '#FC5A5A',
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
